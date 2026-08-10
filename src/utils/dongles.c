f4835737aae6fd82feac04a1b0cfba76b47d47d6
naportel <naportel@student.42.rio>
2026-08-10 15:02:59 -0300

    Update: Fixed the way lock_dongle works, preventing possible deadlocks

--- src/utils/dongles.c
+++ src/utils/dongles.c
@@ -6,7 +6,7 @@
 /*   By: naportel <naportel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/05 14:05:13 by naportel          #+#    #+#             */
-/*   Updated: 2026/08/05 14:58:33 by naportel         ###   ########.fr       */
+/*   Updated: 2026/08/10 12:34:08 by naportel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,29 @@
 
 void	lock_dongles(t_coder *coder)
 {
-	pthread_mutex_lock(&coder->left_dongle->dongle_mutex);
+    t_dongle *first;
+    t_dongle *second;
+    
+    if (coder->left_dongle < coder->right_dongle)
+    {
+        first = coder->left_dongle;
+        second = coder->right_dongle;
+    }
+    else
+    {
+        first = coder->right_dongle;
+        second = coder->left_dongle;
+    }
+	pthread_mutex_lock(&first->mutex);
 	print_log(coder, "has taken a dongle");
-	pthread_mutex_lock(&coder->right_dongle->dongle_mutex);
+	pthread_mutex_lock(&second->mutex);
 	print_log(coder, "has taken a dongle");
 	return ;
 }
 
 void	unlock_dongles(t_coder *coder)
 {
-	pthread_mutex_unlock(&coder->left_dongle->dongle_mutex);
-	pthread_mutex_unlock(&coder->right_dongle->dongle_mutex);
+	pthread_mutex_unlock(&coder->left_dongle->mutex);
+	pthread_mutex_unlock(&coder->right_dongle->mutex);
 	return ;
 }

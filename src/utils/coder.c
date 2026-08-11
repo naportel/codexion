127d6bd16cc362a04f72b66323466c5443c8b51f
naportel <naportel@student.42.rio>
2026-08-11 14:33:34 -0300

    Fix: small typo on usleep

--- src/utils/coder.c
+++ src/utils/coder.c
@@ -6,7 +6,7 @@
 /*   By: naportel <naportel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/05 14:29:45 by naportel          #+#    #+#             */
-/*   Updated: 2026/08/05 14:29:51 by naportel         ###   ########.fr       */
+/*   Updated: 2026/08/11 11:50:02 by naportel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	coder_compile(t_coder *coder)
 {
 		print_log(coder, "is compiling");
 		coder->last_comp = get_current_time();
-		usleep(coder->table->compile_time * 1000);
+		usleep(coder->table->comp_time * 1000);
 		pthread_mutex_lock(&coder->coder_mutex);
 		coder->last_comp = get_current_time();
 		coder->comps_done++;

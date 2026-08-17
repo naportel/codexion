9b73f647f3f224c763be96653de7e34666b999eb
naportel <naportel@student.42.rio>
2026-08-17 16:00:58 -0300

    Fix: data race issue on print_log

--- src/utils/coder.c
+++ src/utils/coder.c
@@ -6,7 +6,7 @@
 /*   By: naportel <naportel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/05 14:29:45 by naportel          #+#    #+#             */
-/*   Updated: 2026/08/14 15:31:28 by naportel         ###   ########.fr       */
+/*   Updated: 2026/08/17 15:45:45 by naportel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 void	coder_compile(t_coder *coder)
 {
 	print_log(coder, "is compiling", 0);
+	pthread_mutex_lock(&coder->coder_mutex);
 	coder->last_comp = get_time();
 	usleep(coder->table->comp_time * 1000);
-	pthread_mutex_lock(&coder->coder_mutex);
 	coder->comps_done++;
 	pthread_mutex_unlock(&coder->coder_mutex);
 	return ;
@@ -29,12 +29,17 @@ void	print_log(t_coder *coder, char *msg, int type)
 	long	time;
 
 	table = coder->table;
-	pthread_mutex_lock(&table->log_mutex);
-	time = get_time() - table->start_time;
-	printf("%ld %d %s\n", time, coder->id + 1, msg);
-	if (type == 1)
-		usleep(coder->table->debug_time * 1000);
-	else if (type == 2)
-		usleep(coder->table->refactor_time * 1000);
-	pthread_mutex_unlock(&table->log_mutex);
+	if (table->simulation_run)
+	{
+		pthread_mutex_lock(&table->log_mutex);
+		time = get_time() - table->start_time;
+		printf("%ld %d %s\n", time, coder->id + 1, msg);
+		pthread_mutex_unlock(&table->log_mutex);
+		if (type == 1)
+			usleep(table->debug_time * 1000);
+		else if (type == 2)
+			usleep(table->refactor_time * 1000);
+	}
+	else
+		return ;
 }
